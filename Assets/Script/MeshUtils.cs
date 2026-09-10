a0d45b97c5c4f534780f1ea3d45f676436a89844
CaielonCoder <caieloncoder@disroot.org>
2026-09-10 13:01:15 -0300

    Better blockers unions

--- Assets/Script/MeshUtils.cs
+++ Assets/Script/MeshUtils.cs
@@ -49,56 +49,79 @@ public class MeshUtils
         Vector3 startPoint, Vector3 startForward, Vector3 endPoint, Vector3 endForward, 
         float radius, int subdivisions = 4, int faces = 16)
     {
-        Vector3 subPStart = startPoint;
-        Vector3 subFStart = startForward.normalized;
-        Vector3 subPEnd = endPoint;
-        Vector3 subFEnd = endForward.normalized;
-
-        Vector3 subUpStart = subFStart - subFEnd + Vector3.up * 0.1f;
-        Vector3 subUpEnd = subUpStart;
-
-        Vector3.OrthoNormalize(ref subFStart, ref subUpStart);
-        Vector3.OrthoNormalize(ref subFEnd, ref subUpEnd);
-
-        Vector3 subRightStart = Vector3.Cross(subFStart, subUpStart);
-        Vector3 subRightEnd = Vector3.Cross(subFEnd, subUpEnd);
-
         List<Vector3> vertices = new List<Vector3>(mesh.vertices);
         List<Vector3> normals = new List<Vector3>(mesh.normals);
-        Vector3 normalStart;
-        Vector3 normalEnd;
-        float angle = 0;
-        float angleStep = 2 * Mathf.PI / faces;
-
-        int startIndex = mesh.vertices.Length;
         List<int> triangles = new List<int>(mesh.triangles);
+        startForward.Normalize();
+        endForward.Normalize();
 
-        for (int i = 0; i < faces; i++)
+        Vector3 center = CalculateCircleCenter(startPoint, startForward, endPoint, endForward);
+        Vector3 sleprStart = startPoint - center;
+        Vector3 sleprEnd = endPoint - center;
+
+        for (int subIndex = 0; subIndex < subdivisions; subIndex++)
         {
-            normalStart = subUpStart * Mathf.Sin(angle) + subRightStart * Mathf.Cos(angle);
-            vertices.Add(normalStart * radius + startPoint);
-            normals.Add(normalStart);
+            float tStart = 1f * subIndex / subdivisions;
+            Vector3 subPStart = Vector3.Slerp(sleprStart, sleprEnd, tStart) + center;
+            Vector3 subFStart = Vector3.Slerp(startForward, endForward, tStart);
 
-            normalEnd = subUpEnd * Mathf.Sin(angle) + subRightEnd * Mathf.Cos(angle);
-            vertices.Add(normalEnd * radius + endPoint);
-            normals.Add(normalEnd);
+            float tEnd = (1f + subIndex) / subdivisions;
+            Vector3 subPEnd = Vector3.Slerp(sleprStart, sleprEnd, tEnd) + center;
+            Vector3 subFEnd = Vector3.Slerp(startForward, endForward, tEnd);
 
-            angle += angleStep;
+            Vector3 subUpStart = subFStart - subFEnd + Vector3.up * 0.1f;
+            Vector3 subUpEnd = subUpStart;
 
-            triangles.Add(startIndex + i * 2);
-            triangles.Add(startIndex + i * 2 + 1);
-            triangles.Add(startIndex + i * 2 + 3);
-            triangles.Add(startIndex + i * 2);
-            triangles.Add(startIndex + i * 2 + 3);
-            triangles.Add(startIndex + i * 2 + 2);
+            Vector3.OrthoNormalize(ref subFStart, ref subUpStart);
+            Vector3.OrthoNormalize(ref subFEnd, ref subUpEnd);
+
+            Vector3 subRightStart = Vector3.Cross(subFStart, subUpStart);
+            Vector3 subRightEnd = Vector3.Cross(subFEnd, subUpEnd);
+
+            Vector3 normalStart;
+            Vector3 normalEnd;
+            float angle = 0;
+            float angleStep = 2 * Mathf.PI / faces;
+
+            int startIndex = vertices.Count;
+
+            for (int i = 0; i < faces; i++)
+            {
+                normalStart = subUpStart * Mathf.Sin(angle) + subRightStart * Mathf.Cos(angle);
+                vertices.Add(normalStart * radius + subPStart);
+                normals.Add(normalStart);
+
+                normalEnd = subUpEnd * Mathf.Sin(angle) + subRightEnd * Mathf.Cos(angle);
+                vertices.Add(normalEnd * radius + subPEnd);
+                normals.Add(normalEnd);
+
+                angle += angleStep;
+
+                triangles.Add(startIndex + i * 2);
+                triangles.Add(startIndex + i * 2 + 1);
+                triangles.Add(startIndex + i * 2 + 3);
+                triangles.Add(startIndex + i * 2);
+                triangles.Add(startIndex + i * 2 + 3);
+                triangles.Add(startIndex + i * 2 + 2);
+            }
+
+            triangles[triangles.Count - 4] = startIndex + 1;
+            triangles[triangles.Count - 2] = startIndex + 1;
+            triangles[triangles.Count - 1] = startIndex;
         }
-
-        triangles[triangles.Count - 4] = startIndex + 1;
-        triangles[triangles.Count - 2] = startIndex + 1;
-        triangles[triangles.Count - 1] = startIndex;
 
         mesh.vertices = vertices.ToArray();
         mesh.normals = normals.ToArray();
         mesh.triangles = triangles.ToArray();
+    }
+
+    public static Vector3 CalculateCircleCenter(Vector3 p1, Vector3 t1, Vector3 p2, Vector3 t2)
+    {
+        Vector3 dis = p2 - p1;
+        Vector3 normal = Vector3.Cross(dis, t1).normalized;
+        Vector3 n1 = Vector3.Cross(normal, t1).normalized;
+        Vector3 n2 = Vector3.Cross(normal, t2).normalized;
+        float t = Vector3.Dot(Vector3.Cross(dis, n2), normal) / Vector3.Dot(Vector3.Cross(n1, n2), normal);
+        return p1 + t * n1;
     }
 }
